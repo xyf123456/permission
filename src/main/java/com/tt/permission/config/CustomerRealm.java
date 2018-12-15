136679196f4143c30378adf00d95bfa7b4a8669e
xyf <905231404@qq.com>
2018-12-15 21:10:13 +0800

    添加了关于自定义Realm的相关测试

---
+++ src/main/java/com/tt/permission/config/CustomerRealm.java
@@ -0,0 +1,117 @@
+package com.tt.permission.config;
+
+import com.tt.permission.dao.ResourceRepository;
+import com.tt.permission.dao.UserRepository;
+import com.tt.permission.pojo.User;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+/**
+ * @ Author     ：xyf
+ * @ Date       ：Created in 2018/12/15 0015下午 8:56
+ * @ Description：
+ * @ Modified By：xyf
+ * @Version:
+ */
+public class CustomerRealm extends AuthorizingRealm {
+
+    Map<String, String> userMap = new HashMap<>();
+
+    {
+        userMap.put("admin", "123456");
+        super.setName("customerRealm");
+    }
+
+    /**
+     * @ Description: 授权认证
+     * @params: * @Param: principals
+     * @return:org.apache.shiro.authz.AuthorizationInfo
+     **/
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        // 获取当前登录获得认证的用户
+        String username = (String) principals.getPrimaryPrincipal();
+//       模拟从数据库或者缓存中获取用户拥有的角色
+        Set<String> roles = findRolesByUsername(username);
+        //       模拟从数据库或者缓存中获取角色拥有的权限
+        Set<String> permissions = findPermissionsByRolename(username);
+//        创建授权认证对象SimpleAuthorizationInfo
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+//        授权认证对象设置相关的角色
+        authorizationInfo.setRoles(roles);
+//        授权认证对象设置相关的角色拥有的权限
+        authorizationInfo.setStringPermissions(permissions);
+        return authorizationInfo;
+    }
+
+    /**
+     * create by: xyf
+     * description: 模拟从数据库或者缓存中获取用户拥有的权限
+     * create time: 下午 9:06 2018/12/15 0015
+     *
+     * @Param: username
+     * @return java.util.Set<java.lang.String>
+     */
+    private Set<String> findPermissionsByRolename(String username) {
+        Set<String> sets = new HashSet<>();
+        sets.add("user:add");
+        sets.add("user:delete");
+        sets.add("order:add");
+        sets.add("order:delete");
+        return sets;
+    }
+
+
+    /**
+     * create by: xyf
+     * description:  模拟从数据库或者缓存中获取用户拥有的角色
+     * create time: 下午 9:05 2018/12/15 0015
+     *
+     * @Param: username
+     * @return java.util.Set<java.lang.String>
+     */
+    private Set<String> findRolesByUsername(String username) {
+        Set<String> sets = new HashSet<>();
+        sets.add("admin");
+        sets.add("user");
+        return sets;
+    }
+
+    /**
+     * @ Description: 身份认证
+     * @params: * @Param: token
+     * @return:org.apache.shiro.authc.AuthenticationInfo
+     **/
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 编写Shiro判断逻辑，判断账号和密码
+        String username = (String) authenticationToken.getPrincipal();
+        // 1、判断账号
+        String password = findPwdByUsername(username);
+        //2、判断密码
+        if (password == null) {
+            return null;
+        }
+        // 认证后做授权处理，需要将获得认证的用户对象赋值给principal，授权处理时会用到
+        SimpleAuthenticationInfo authorizationInfo = new SimpleAuthenticationInfo("admin", password, "customerRealm");
+        return authorizationInfo;
+    }
+
+    /**
+     * create by: xyf
+     * description: 模拟从数据库获取用户的凭证
+     * create time: 下午 8:59 2018/12/15 0015
+     *
+     * @return java.lang.String
+     * @Param: username
+     */
+    private String findPwdByUsername(String username) {
+        return userMap.get(username);
+    }
+}
---
+++ src/test/java/com/tt/permission/config/CustomerRealmTest.java
@@ -0,0 +1,47 @@
+package com.tt.permission.config;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerRealmTest {
+
+    @Test
+    public void testAuthentication() {
+//        创建CustomerRealm对象
+        CustomerRealm customerRealm = new CustomerRealm();
+
+
+//        1、构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+//        SecurityManager通过设置不同类型的Realm来进行主体的验证
+        defaultSecurityManager.setRealm(customerRealm);
+
+
+//        2、主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+//        获取认证的主体
+        Subject subject = SecurityUtils.getSubject();
+//        通过用户名和密码获取UsernamePasswordToken
+        UsernamePasswordToken token = new UsernamePasswordToken("admin", "123456");
+//        通过token进行登录认证
+        subject.login(token);
+//        打印出是否认证成功
+        System.out.println("isAuthenticated(是否认证):" + subject.isAuthenticated());
+//        检查角色
+        subject.checkRole("admin");
+//        检查权限
+//        subject.checkPermission("/permission");
+        subject.checkPermissions("user:add","user:delete");
+    }
+
+}
